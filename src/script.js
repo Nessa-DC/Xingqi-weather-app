12c06708873dde15dc3b9c7e6c9ac15a910bad03
Nessa-DC <decesarev@gmail.com>
2021-04-18 23:05:38 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -4,10 +4,13 @@ let h2 = document.querySelector("h2");
 
 let date = now.getDate();
 let hours = now.getHours();
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
+ if (hours < 10) {
+        hours = `0${hours}`;
+    }
+ let minutes = now.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
 
 let days = [
   "Sunday",
